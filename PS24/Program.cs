0f7dd4412c65ae5b9220ad204b2e898b9188c4fe
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-21 15:01:58 +0530

    Add project files.

---
+++ PS24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            for (x = 1; x <= 5; x++)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+                Console.Read();
+               
+                
+            
+            Console.Read();
+        }
+    }
+}
